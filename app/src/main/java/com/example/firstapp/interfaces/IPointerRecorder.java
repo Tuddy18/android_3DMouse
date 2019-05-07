ffc7ad52456936141ad7191f0d612be18c0857c6
tuddy <tuddy18_26@yahoo.com>
2019-05-07 22:12:03 +0300

    added 2 main interfaces

--- app/src/main/java/com/example/firstapp/interfaces/IPointerRecorder.java
+++ app/src/main/java/com/example/firstapp/interfaces/IPointerRecorder.java
@@ -2,6 +2,8 @@ package com.example.firstapp.interfaces;
 
 public interface IPointerRecorder {
     void recordPosition(int x, int y, int z);
+    boolean isDrawing();
+    boolean isMoving();
     void setIsDrawing(boolean value);
     void setIsMoving(boolean value);
     void reposition();
