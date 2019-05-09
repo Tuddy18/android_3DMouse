8e17e38967d5c4cf224b8a785b8f71f401364067
SinziToma <sanzi_tm@yahoo.com>
2019-05-09 16:20:26 +0300

    about

--- app/src/main/java/com/example/firstapp/activities/AboutActivity.java
+++ app/src/main/java/com/example/firstapp/activities/AboutActivity.java
@@ -1,9 +1,6 @@
 package com.example.firstapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import com.example.firstapp.R;
 
 public class AboutActivity extends AppCompatActivity {
 
